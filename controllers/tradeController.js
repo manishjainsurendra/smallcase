d7057bd71361ebf7f3c6b9cb3251c4561abd6e1a
KiranNinave <ninavekirankn@gmail.com>
2019-11-26 16:55:09 +0530

    controller structure updated

---
+++ controllers/tradeController.js
@@ -0,0 +1,39 @@
+exports.getTrades = async (req, res) => {
+  try {
+  } catch (err) {
+    console.log(err);
+    res.sendServerError();
+  }
+};
+
+exports.addTrade = async (req, res) => {
+  try {
+  } catch (err) {
+    console.log(err);
+    res.sendServerError();
+  }
+};
+
+exports.updateTradeById = async (req, res) => {
+  try {
+  } catch (err) {
+    console.log(err);
+    res.sendServerError();
+  }
+};
+
+exports.deleteTradeById = async (req, res) => {
+  try {
+  } catch (err) {
+    console.log(err);
+    res.sendServerError();
+  }
+};
+
+exports.getReturns = async (req, res) => {
+  try {
+  } catch (err) {
+    console.log(err);
+    res.sendServerError();
+  }
+};
--- routes/tradeRoutes.js
+++ routes/tradeRoutes.js
@@ -1,3 +1,27 @@
 const router = require("express").Router();
 
+// controller
+const tradeController = require("../controllers/tradeController");
+
+// validator
+const validator = require("../validators/");
+
+router
+  .route("/")
+  .get(tradeController.getTrades)
+  .post(
+    validator.validateBody(validator.schemas.tradeValidator),
+    tradeController.addTrade
+  );
+
+router
+  .route("/:id")
+  .put(
+    validator.validateBody(validator.schemas.tradeValidator),
+    tradeController.updateTradeById
+  )
+  .delete(tradeController.deleteTradeById);
+
+router.route("/returns").get(tradeController.getReturns);
+
 module.exports = router;
